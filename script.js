fbf57893453cfe582b120a60c92b4f42adb33c5f
takia24 <takiayasmin75@gmail.com>
2025-12-24 12:36:29 +0600

    Add navbar, dark mode, and mobile optimization

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-}
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
+
